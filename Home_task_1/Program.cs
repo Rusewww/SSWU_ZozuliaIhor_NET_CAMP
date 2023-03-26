bfffe0d0e7e93bdab396a2d65407f89b6c6b85b1
unknown <rusewww1@gmail.com>
2023-03-26 11:23:53 +0300

    Fix

--- Home_task_1/Program.cs
+++ Home_task_1/Program.cs
@@ -1,25 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Home_task_1;
+﻿using Home_task_1;
 
-/*SnakeMatrix snake = new SnakeMatrix();
-snake.MakeSnake();
-Console.Write(snake.ToString());*/
+Task_1_1();
+Task_1_2();
 
-int[,] matrix =
+static void Task_1_1()
 {
-    {
-        1, 2, 3, 4
-    },
-    {
-        2, 2, 2, 3
-    },
-    {
-        3, 2, 2, 1
-    },
-    {
-        4, 4, 4, 2
-    }
-};
-ColorMatrix colorMatrix = new ColorMatrix(matrix);
-Console.Write(colorMatrix.ToString());
-colorMatrix.FindColor();
+    Console.WriteLine("|============TASK_1_1============|");
+    var snake = new SnakeMatrix();
+    snake.MakeSnake();
+    Console.Write(snake.ToString());
+    Console.WriteLine("|================================|");
+}
+
+static void Task_1_2()
+{
+    Console.WriteLine("|============TASK_1_2============|");
+    /*int[,] matrix = {{1, 2, 3, 4}, {2, 2, 2, 3}, {3, 2, 2, 1}, {4, 4, 4, 2}};
+    ColorMatrix colorMatrix = new ColorMatrix(matrix);*/
+    var colorMatrix = new ColorMatrix(5, 5);
+    colorMatrix.FillMatrixRandom();
+    Console.Write(colorMatrix.ToString());
+    colorMatrix.FindColor();
+    Console.WriteLine("|================================|");
+}
+
