522ec68174365b15c86372870a1b948fd13d2af5
unknown <rusewww1@gmail.com>
2023-04-04 12:55:35 +0300

    Exercise2_fin

--- Home_task_3/Exercise_2/Program.cs
+++ Home_task_3/Exercise_2/Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Exercise_2;
+﻿using Exercise_2;
 
-string text = "Baa nan baa nan bana nan";
-Console.WriteLine(StringManipulator.FindSecondIndex(text, "nan"));
+string text = "Baa nan dBaa nan bana nan Ban Ape";
+Console.WriteLine("Текст: " + text);
+Console.WriteLine($"Iндекс другого входження слова nan: { StringManipulator.FindSecondIndex(text, "nan")}");
+Console.WriteLine($"Кiлькiсть слiв, якi починаються з великої лiтери: {StringManipulator.CountWordsStartingWithCapitalLetter(text)}");
+Console.WriteLine($"Строка, де слова з подвоєннями замiненi на 0: {StringManipulator.ReplaceWordWithDoubleLetters(text, "0")}");
--- Home_task_3/Exercise_2/StringManipulator.cs
+++ Home_task_3/Exercise_2/StringManipulator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise_2
@@ -28,6 +29,71 @@ namespace Exercise_2
                     return secondIndex;
                 }
             }
+        }
+
+        public static int? CountWordsStartingWithCapitalLetter(string text)
+        {
+            int count = 0;
+            Regex regex = new Regex(@"\b[A-Z][a-z]*\b");
+            MatchCollection matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                if (char.IsUpper(match.Value[0]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static string ReplaceWordWithDoubleLetters(string text, string replace)
+        {
+            StringBuilder output = new StringBuilder();
+            StringBuilder currentWord = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (char.IsLetter(c))
+                {
+                    currentWord.Append(c);
+                }
+                else
+                {
+                    if (HasDoubleLetters(currentWord))
+                    {
+                        output.Append(replace);
+                    }
+                    else
+                    {
+                        output.Append(currentWord);
+                    }
+                    output.Append(c);
+                    currentWord.Clear();
+                }
+            }
+            if (HasDoubleLetters(currentWord))
+            {
+                output.Append(replace);
+            }
+            else
+            {
+                output.Append(currentWord);
+            }
+            return output.ToString();
+        }
+
+        private static bool HasDoubleLetters(StringBuilder word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
