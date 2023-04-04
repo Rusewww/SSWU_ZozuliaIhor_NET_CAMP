228c19a2ddc8da8b2754946b3c59d6a54a4159b8
unknown <rusewww1@gmail.com>
2023-04-04 12:29:04 +0300

    Exercise2_1

---
+++ Home_task_3/Exercise_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise_2;
+
+string text = "Baa nan baa nan bana nan";
+Console.WriteLine(StringManipulator.FindSecondIndex(text, "nan"));
---
+++ Home_task_3/Exercise_2/StringManipulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    internal class StringManipulator
+    {
+        public static int? FindSecondIndex(string text, string subString)
+        {
+            int firstIndex = text.IndexOf(subString);
+            if (firstIndex == -1)
+            {
+                return null;
+            }
+            else
+            {
+                int secondIndex = text.IndexOf(subString, firstIndex + 1);
+                if (secondIndex == -1)
+                {
+                    return null;
+                }
+                else
+                {
+                    return secondIndex;
+                }
+            }
+        }
+    }
+}
