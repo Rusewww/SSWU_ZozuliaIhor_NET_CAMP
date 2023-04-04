2a17f6e764eb35b17cfd246c3084c48362924ced
unknown <rusewww1@gmail.com>
2023-04-04 12:57:03 +0300

    Update Program.cs

--- Home_task_3/Exercise_2/Program.cs
+++ Home_task_3/Exercise_2/Program.cs
@@ -1,7 +1,14 @@
-﻿using Exercise_2;
-
-string text = "Baa nan dBaa nan bana nan Ban Ape";
-Console.WriteLine("Текст: " + text);
-Console.WriteLine($"Iндекс другого входження слова nan: { StringManipulator.FindSecondIndex(text, "nan")}");
-Console.WriteLine($"Кiлькiсть слiв, якi починаються з великої лiтери: {StringManipulator.CountWordsStartingWithCapitalLetter(text)}");
-Console.WriteLine($"Строка, де слова з подвоєннями замiненi на 0: {StringManipulator.ReplaceWordWithDoubleLetters(text, "0")}");
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "Baa nan dBaa nan bana nan Ban Ape";
+            Console.WriteLine("Текст: " + text);
+            Console.WriteLine($"Iндекс другого входження слова nan: {StringManipulator.FindSecondIndex(text, "nan")}");
+            Console.WriteLine($"Кiлькiсть слiв, якi починаються з великої лiтери: {StringManipulator.CountWordsStartingWithCapitalLetter(text)}");
+            Console.WriteLine($"Строка, де слова з подвоєннями замiненi на 0: {StringManipulator.ReplaceWordWithDoubleLetters(text, "0")}");
+        }
+    }
+}
