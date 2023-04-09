b37fb93f6917dc7c097459d607028b0b15ae3356
unknown <rusewww1@gmail.com>
2023-04-10 01:26:02 +0300

    Home_task_4-Exercise_1

---
+++ Home_task_4/Exercise_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Демонстрація на українській
+            string[] textUa = {"Це (речення 1).А це", " (речення 2)!", "А також (речення 3 без закритої дужки.", "Текст."};
+            var stringTextUa = new TextEditor(new List<string>(textUa));
+            
+            Console.WriteLine("Сортування речень по строкам:");
+            foreach (string str in stringTextUa.SortSentencesInArray())
+            {
+                Console.WriteLine("\t" + str);
+            }
+            
+            Console.WriteLine("\nВиведення речень, якi мають у собi текст в дужках:");
+            foreach (string bracket in stringTextUa.FindBrackets())
+            {
+                Console.WriteLine("\t" + bracket);
+            }
+            
+            //Демонстрація на англійській
+            string[] textEn =
+            {
+                "This is (sentence 1).And this", "(sentence 2)!",
+                "As well as (sentence 3 without a closed parenthesis.", "Text."
+            };
+            var stringTextEn = new TextEditor(new List<string>(textEn));
+            
+            Console.WriteLine("\nSorting sentences by lines:");
+            foreach (string str in stringTextEn.SortSentencesInArray())
+            {
+                Console.WriteLine("\t" + str);
+            }
+
+            Console.WriteLine("\nPrint sentences that contain text in brackets:");
+            foreach (string bracket in stringTextEn.FindBrackets())
+            {
+                Console.WriteLine("\t" + bracket);
+            }
+        }
+    }
+}
---
+++ Home_task_4/Exercise_1/TextEditor.cs
@@ -0,0 +1,55 @@
+﻿namespace Exercise_1
+{
+    public class TextEditor
+    {
+        private List<string> _text;
+        public List<string> Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        public TextEditor(List<string> text)
+        {
+            Text = text;
+        }
+
+        public List<string> SortSentencesInArray()
+        {
+            List<string> sentences = new List<string>();
+            string currentSentence = "";
+
+            foreach (string line in Text)
+            {
+                string[] lineSentences = line.Split('.', '!', '?');
+                
+                lineSentences[0] = currentSentence + lineSentences[0];
+                if (!line.EndsWith('.') && !line.EndsWith('!') && !line.EndsWith('?'))
+                {
+                    currentSentence = lineSentences[lineSentences.Length - 1];
+                    lineSentences = lineSentences.Take(lineSentences.Length - 1).ToArray();
+                }
+                else
+                {
+                    currentSentence = "";
+                }
+                sentences.AddRange(lineSentences);
+            }
+            Text = sentences;
+            return sentences;
+        }
+
+        public List<string> FindBrackets()
+        {
+            var sentencesInBrackets = new List<string>();
+            foreach (var sentence in Text)
+            {
+                if (sentence.Contains("(") && sentence.Contains(")"))
+                {
+                    sentencesInBrackets.Add(sentence);
+                }
+            }
+            return sentencesInBrackets;
+        }
+    }
+}
