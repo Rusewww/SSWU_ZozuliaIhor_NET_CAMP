e4e7e9c56227408dde2ac752a68181a7e51ff328
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-24 05:55:26 +0300

    Update Program.cs

--- Home_task_4/Exercise_3/Program.cs
+++ Home_task_4/Exercise_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Exercise_3
 {
     class Program
-    {
+    {// Не всі вимоги враховані. Інформація повинна виводитись за квартал і дати мають бути за іншим форматом.
         static void Main(string[] args)
         {
             ConsoleInterface.Start();
