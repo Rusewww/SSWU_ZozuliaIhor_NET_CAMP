06c3893bd64f784267446b24df4db39176c18c50
unknown <rusewww1@gmail.com>
2023-04-29 23:49:59 +0300

    Home_task_6-Exercise_2-SortingArray

---
+++ Home_task_6/Exercuse_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 4, 1, 5 };
+            int[] array2 = { 2, 7, 3 };
+            int[] array3 = { 6, 9, 8 };
+
+            foreach (int number in SortedArrayMerger.MergeSortedArrays(array1, array2, array3))
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
---
+++ Home_task_6/Exercuse_2/SortedArrayMerger.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise_2
+{
+    public class SortedArrayMerger
+    {
+        public static IEnumerable<int> MergeSortedArrays(params int[][] arrays)
+        {
+            var mergedArray = arrays.SelectMany(x => x).OrderBy(x => x);
+            foreach (var item in mergedArray)
+            {
+                yield return item;
+            }
+        }
+    }
+}
