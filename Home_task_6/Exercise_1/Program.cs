ba5405a0b8e23ee89115ebd13482b6430a5c8ece
unknown <rusewww1@gmail.com>
2023-04-29 14:36:35 +0300

    Home_task_6-Exercise_1-Spiral_matrix

---
+++ Home_task_6/Exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+            };
+
+            SpiralMatrix spiralMatrix = new SpiralMatrix(matrix);
+
+            foreach (int num in spiralMatrix)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
---
+++ Home_task_6/Exercise_1/SpiralMatrix.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace Exercise_1
+{
+    public class SpiralMatrix : IEnumerable<int>
+    {
+        private int[,] _matrix;
+
+        public SpiralMatrix(int[,] matrix)
+        {
+            this._matrix = (int[,])matrix.Clone();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int size = _matrix.GetLength(0);
+            int row = 0, col = 0;
+            yield return _matrix[row, col];
+            for (int i = 0; i < size * size - 1; i++)
+            {
+                if ((row + col) % 2 == 0)
+                {
+                    if (col == size - 1)
+                    {
+                        row++;
+                    }
+                    else if (row == 0)
+                    {
+                        col++;
+                    }
+                    else
+                    {
+                        row--;
+                        col++;
+                    }
+                }
+                else
+                {
+                    if (row == size - 1)
+                    {
+                        col++;
+                    }
+                    else if (col == 0)
+                    {
+                        row++;
+                    }
+                    else
+                    {
+                        row++;
+                        col--;
+                    }
+                }
+                yield return _matrix[row, col];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
