a8f336ef426c2c621a6fabc7c65982478ebd69e2
unknown <rusewww1@gmail.com>
2023-04-30 01:10:53 +0300

    Home_work_6-Exercise_3-Unique

---
+++ Home_task_6/Exercise_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "do Lorem ipsum do. dolor do. sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+            var uniqueWords = UniqueWordsFinder.GetUniqueWords(text);
+
+            foreach (var word in uniqueWords)
+            {
+                Console.Write(word + " ");
+            }
+        }
+    }
+}
+
---
+++ Home_task_6/Exercise_3/UniqueWordsFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise_3
+{
+    public class UniqueWordsFinder
+    {
+        public static IEnumerable<string> GetUniqueWords(string text)
+        {
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var uniqueWords = new HashSet<string>();
+
+            foreach (var word in words)
+            {
+                var cleanedWord =
+                    Regex.Replace(word, @"[^\w\s]", ""); //Видаляємо зайві символи, які можуть залишитися в словах
+
+                if (uniqueWords.Add(cleanedWord))
+                {
+                    yield return cleanedWord;
+                }
+            }
+        }
+    }
+}
