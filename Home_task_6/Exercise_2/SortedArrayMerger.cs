dd6b47e9804872477391914808523ebdf6252502
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 16:01:30 +0300

    Update SortedArrayMerger.cs

--- Home_task_6/Exercise_2/SortedArrayMerger.cs
+++ Home_task_6/Exercise_2/SortedArrayMerger.cs
@@ -3,13 +3,14 @@
     public class SortedArrayMerger
     {
         private int[][] _arrays;
+        //краще на вході мати params
         public SortedArrayMerger(int[][] arrays)
         {
             this._arrays = (int[][]) arrays.Clone();
         }
 
         public IEnumerable<int> MergeSortedArrays()
-        {
+        {// яка ефективність yield?
             var mergedArray = _arrays.SelectMany(x => x).OrderBy(x => x);
             foreach (var item in mergedArray)
             {
