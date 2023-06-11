b7828d0298d6b5ca2f8738a09e71db74add5b912
unknown <rusewww1@gmail.com>
2023-06-11 23:08:04 +0300

    Exercise_1-Sorting

---
+++ Home_task_11/Exercise_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Приклад використання зі строками
+            string[] array = { "apple", "banana", "cherry", "date", "elderberry" };
+
+            Console.WriteLine("Сортування з першим елементом в якості опорного:");
+            QuickSort.Sort(array, (x, y) => x.CompareTo(y));
+            PrintArray(array);
+
+            Console.WriteLine("\nСортування з довільним елементом в якості опорного:");
+            QuickSort.Sort(array, (x, y) => y.Length.CompareTo(x.Length));
+            PrintArray(array);
+
+            Console.WriteLine("\nСортування з медіаною в якості опорного:");
+            QuickSort.Sort(array, (x, y) => x[0].CompareTo(y[0]));
+            PrintArray(array);
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ Home_task_11/Exercise_1/QuickSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public static class QuickSort
+    {
+        public static void Sort<T>(T[] array, Func<T, T, int> compare)
+        {
+            QuickSortRecursive(array, 0, array.Length - 1, compare);
+        }
+
+        private static void QuickSortRecursive<T>(T[] array, int left, int right, Func<T, T, int> compare)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right, compare);
+                QuickSortRecursive(array, left, pivotIndex - 1, compare);
+                QuickSortRecursive(array, pivotIndex + 1, right, compare);
+            }
+        }
+
+        private static int Partition<T>(T[] array, int left, int right, Func<T, T, int> compare)
+        {
+            T pivot = array[left];
+            int i = left;
+
+            for (int j = left + 1; j <= right; j++)
+            {
+                if (compare(array[j], pivot) < 0)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, left, i);
+
+            return i;
+        }
+
+        private static void Swap<T>(T[] array, int i, int j)
+        {
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
